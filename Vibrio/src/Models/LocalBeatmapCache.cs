3f55b217cffbd80f44b5d6719ab67aabe64da762
Jagan Prem <notjagan@gmail.com>
2023-10-26 19:46:43 -0700

    Store cached beatmaps with .osu file extension

--- Vibrio/src/Models/LocalBeatmapCache.cs
+++ Vibrio/src/Models/LocalBeatmapCache.cs
@@ -2,32 +2,25 @@
 using osu.Game.Beatmaps;
 using vibrio.src.Exceptions;
 
-namespace vibrio.src.Models
-{
-    public class LocalBeatmapCache : IBeatmapProvider
-    {
+namespace vibrio.src.Models {
+    public class LocalBeatmapCache : IBeatmapProvider {
         private readonly string cacheDirectory;
         private readonly string osuRootUrl;
 
-        public LocalBeatmapCache(IConfiguration config)
-        {
+        public LocalBeatmapCache(IConfiguration config) {
             cacheDirectory = config["CacheDirectory"];
-            if (cacheDirectory == null)
-            {
+            if (cacheDirectory == null) {
                 throw new MissingConfigurationException("No configuration value for beatmap cache directory provided.");
             }
             osuRootUrl = config["OsuRootUrl"];
-            if (osuRootUrl == null)
-            {
+            if (osuRootUrl == null) {
                 throw new MissingConfigurationException("No configuration value for osu! URL provided.");
             }
         }
 
-        public WorkingBeatmap GetBeatmap(int beatmapId)
-        {
-            var beatmapPath = Path.Combine(cacheDirectory, beatmapId.ToString());
-            if (!File.Exists(beatmapPath))
-            {
+        public WorkingBeatmap GetBeatmap(int beatmapId) {
+            var beatmapPath = Path.Combine(cacheDirectory, $"{beatmapId}.osu");
+            if (!File.Exists(beatmapPath)) {
                 new FileWebRequest(beatmapPath, $"{osuRootUrl}/osu/{beatmapId}").Perform();
                 Console.WriteLine("hello");
             }
