aa540ba80a61dd27dd2992307015d95506251e1f
Jagan Prem <notjagan@gmail.com>
2023-10-30 16:24:00 -0700

    Add endpoint for performance calculation on replay/beatmap file combination

--- Vibrio/Controllers/PerformanceController.cs
+++ Vibrio/Controllers/PerformanceController.cs
@@ -75,5 +75,28 @@ namespace Vibrio.Controllers {
             var attributes = DifficultyController.GetDifficulty(beatmap, info.Mods);
             return (OsuPerformanceAttributes)new OsuPerformanceCalculator().Calculate(info, attributes);
         }
+
+        [HttpPost("replay")]
+        public async Task<ActionResult<OsuPerformanceAttributes>> GetPerformance(IFormFile replay, IFormFile beatmap) {
+            var b = await beatmap.LoadBeatmap();
+            if (b.Beatmap.HitObjects.Count == 0) {
+                return BadRequest("Invalid/empty beatmap file");
+            }
+
+            var decoder = new BeatmapLegacyScoreDecoder(b);
+            using var stream = new MemoryStream();
+            await replay.CopyToAsync(stream);
+            stream.Seek(0, SeekOrigin.Begin);
+
+            ScoreInfo info;
+            try {
+                info = decoder.Parse(stream).ScoreInfo;
+            } catch (BeatmapMismatchException ex) {
+                return BadRequest(ex.Message);
+            }
+
+            var attributes = DifficultyController.GetDifficulty(b, info.Mods);
+            return (OsuPerformanceAttributes)new OsuPerformanceCalculator().Calculate(info, attributes);
+        }
     }
 }
