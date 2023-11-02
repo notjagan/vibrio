d30f655cb04180317a0f053440ca77205a5b306a
Jagan Prem <notjagan@gmail.com>
2023-11-01 17:59:32 -0700

    Add tests for temporary beatmap caching

---
+++ Vibrio.Tests/Tests/TemporaryBeatmapCacheTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using Vibrio.Models;
+
+namespace Vibrio.Tests.Tests {
+    public class TemporaryBeatmapCacheTests : IDisposable {
+        private readonly TemporaryBeatmapCache cache;
+
+        public TemporaryBeatmapCacheTests() {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.Development.json")
+                .Build();
+            cache = new TemporaryBeatmapCache(config);
+            cache.ClearCache();
+        }
+
+        public void Dispose() {
+            cache.ClearCache();
+            GC.SuppressFinalize(this);
+        }
+
+        [Theory]
+        [InlineData(1001682)]
+        [InlineData(2042429)]
+        public void Lookup_beatmap(int beatmapId) {
+            Assert.False(cache.HasBeatmap(beatmapId));
+            var beatmap = cache.GetBeatmap(beatmapId);
+            Assert.NotNull(beatmap);
+            Assert.Equal(beatmap.BeatmapInfo.OnlineID, beatmapId);
+            Assert.True(cache.HasBeatmap(beatmapId));
+        }
+    }
+}
